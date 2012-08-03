f100d88274eedc12a84d2ec805a8122bff6cba5c
Valentin Lab <valentin.lab@kalysto.org>
2012-08-03 13:51:16 +0200

    chg: pkg: better description and removed the auto-install feature in the manifest file

--- __openerp__.py
+++ __openerp__.py
@@ -1,16 +1,17 @@
 {
     "name": "Web Google Chart",
     "category" : "Widgets",
-    "description": 'Openerp web chart view using google chart tools.
+    "description": """Openerp web chart view using google chart tools.
 
-Originally based on legacy chart.js from openerp web_chart module.
-This modules replaces all charts by google version. Please do not use 
-in conjunction with web_graph module or ensure that this module depends
-on it so it can be loaded after web_graph to override it.',
+Originally based on legacy ``chart.js`` from openerp ``web_chart`` module.
+This modules replaces all charts by google chart version.
+
+""",
 
     "version": "0.2",
-    ## As it replace web_graph, we declare to be dependent to be sure
-    ## that it is loaded before.
+    ## As it replace web_graph (and that web_graph auto-installs), we
+    ## declare to be dependent to be sure that web_graph is loaded
+    ## before.
     "depends": ['web', 'web_graph'],
     "js": [
         "static/src/js/chart.js"],
@@ -18,5 +19,4 @@ on it so it can be loaded after web_graph to override it.',
     'qweb' : [
         "static/src/xml/*.xml",
     ],
-    "auto_install": True
 }
